9d41d797a966b962c654393e8d6fedb363705e96
kirilok946 <156037647+kirilok946@users.noreply.github.com>
2025-02-13 06:50:44 -0800

    add staff page

---
+++ resources/js/pages/staff/employee_list.jsx
@@ -0,0 +1,412 @@
+import React, { useState, useEffect } from "react";
+import {
+    Card,
+    Row,
+    Col,
+    Table,
+    Input,
+    Button,
+    Switch,
+    Avatar,
+    Typography,
+    Space,
+    Image,
+    message,
+    Flex,
+    Form,
+    Upload,
+} from "antd";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+    EyeOutlined,
+    FileOutlined,
+    SearchOutlined,
+    FileAddOutlined,
+    UploadOutlined,
+    DeleteOutlined,
+} from "@ant-design/icons";
+import { Calendar, momentLocalizer } from "react-big-calendar"; // or fullcalendar-react
+import { format, parse, startOfWeek, getDay } from "date-fns";
+import "react-big-calendar/lib/css/react-big-calendar.css"; // for react-big-calendar
+import moment from "moment";
+
+import { useSelector, useDispatch } from "react-redux";
+import {
+    useFetchEmployeeListQuery,
+    useFetchEventsQuery,
+    useAddDocumentMutation,
+    useSearchDocumentMutation,
+    useDeleteStaffMutation,
+} from "./staff.service";
+import { setEmployee } from "./staff.slice";
+const localizer = momentLocalizer(moment);
+const { Title } = Typography;
+import ShowIcon from "./showIcon";
+
+const Employee_listPage = ({}) => {
+    const dispatch = useDispatch();
+    const { id: user_id } = useParams();
+    const navigate = useNavigate();
+    const [form] = Form.useForm();
+    const employee_list = useSelector((apps) => apps.employee.employee_list);
+
+    const {
+        data: employeeData,
+        isLoading,
+        isSuccess,
+    } = useFetchEmployeeListQuery(user_id, {
+        refetchOnMountOrArgChange: true,
+    });
+    const { data: calendarData } = useFetchEventsQuery(user_id, {
+        refetchOnMountOrArgChange: true,
+    });
+    const [
+        addDocument,
+        { isLoading: isLoadingUpdate, isSuccess: isUpdatingSuccess },
+    ] = useAddDocumentMutation();
+    const [deleteStaff] = useDeleteStaffMutation();
+
+    const [searchDocument] = useSearchDocumentMutation();
+    const [fileList, setFileList] = useState([]);
+    const [str_search, setStr_search] = useState("");
+    const OnSearchStr = async (e) => {
+        setStr_search(e.target.value);
+        const { data } = await searchDocument({
+            str_search: e.target.value,
+            id: user_id,
+        });
+        setFileList(data);
+    };
+    const handleSwitchChange = (checked) => {
+        message.info(
+            `Calendar view is now ${checked ? "enabled" : "disabled"}`
+        );
+    };
+    const OnDeleteStaff = async (id) => {
+        try {
+            const result = await deleteStaff({ user_id, id });
+            dispatch(setEmployee(employee_list.filter((e) => e.id != id)));
+            message.success(`Employee deleted successfully`);
+        } catch (err) {
+            message.error(`Employee delete failed`);
+        }
+    };
+
+    const columns = [
+        {
+            title: "#",
+            dataIndex: "index",
+            render: (_, record, index) => index + 1,
+        },
+        {
+            title: "Profile Picture",
+            dataIndex: "emp_picture",
+            style: { textAlign: "center" },
+            render: (emp_picture) => (
+                <Avatar
+                    src={
+                        emp_picture
+                            ? `/${emp_picture}`
+                            : "/assets/images/default_user.jpg"
+                    }
+                    size={36}
+                    shape="circle"
+                />
+            ),
+        },
+        {
+            title: "Name",
+            dataIndex: "name",
+            style: { textAlign: "center" },
+            render: (name) => <Typography.Text>{name}</Typography.Text>,
+        },
+        {
+            title: "Email",
+            dataIndex: "email",
+            style: { textAlign: "center" },
+        },
+        {
+            title: "Job Title",
+            dataIndex: "job_title",
+            style: { textAlign: "center" },
+            render: (title) => <Typography.Text>{title}</Typography.Text>,
+        },
+        {
+            title: "Action",
+            dataIndex: "action",
+            render: (text, record) => (
+                <Flex justify="start">
+                    <Button
+                        type="primary"
+                        style={{ width: "30%", marginRight: "10px" }}
+                        icon={<EyeOutlined />}
+                        size="small"
+                        onClick={() =>
+                            navigate(`/employee/${record.id}/edit/${user_id}`)
+                        }
+                    />
+                    <Button
+                        type="default"
+                        variant="solid"
+                        color="danger"
+                        style={{ width: "30%" }}
+                        icon={<DeleteOutlined />}
+                        size="small"
+                        onClick={() => OnDeleteStaff(record.id)}
+                    />
+                </Flex>
+            ),
+        },
+    ];
+    const handleDocumentRequest = async ({
+        file,
+        fileList,
+        onSuccess,
+        onError,
+    }) => {
+        try {
+            onSuccess();
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    const handleDocumentChange = ({ fileList: newFileList, file, event }) => {
+        // You can control when to remove the files here
+        // For example, removing files after upload completion
+        const updatedList = newFileList.map((file) => ({
+            ...file,
+            uid: file.uid || `new-${Date.now()}`, // Ensure unique UID
+        }));
+        setFileList(updatedList);
+    };
+    // Transform employee data for the table
+    const [currentPage, setCurrentPage] = useState(1); // current page number
+    const [pageSize, setPageSize] = useState(5); // number of items per page
+    useEffect(() => {
+        if (isSuccess) {
+            setFileList(employeeData?.user?.user_hr_documents);
+            dispatch(setEmployee(employeeData?.employee_list));
+        }
+    }, [isSuccess, setEmployee, employeeData]);
+    const handleDeleteDocument = (file) => {
+        setFileList(
+            fileList.filter((f) => {
+                const deleteIdentifier = file.id || file.uid;
+                const fileIdentifier = f.id || f.uid;
+                return fileIdentifier !== deleteIdentifier;
+            })
+        );
+    };
+    if (isLoading) return <>Loading</>;
+    const totalCount = employeeData.employees?.length || 0;
+    const onPageChange = (page, pageSize) => {
+        setCurrentPage(page); // Update current page
+        setPageSize(pageSize); // Update page size if the user changes it
+    };
+    const onFinish = async (values, id) => {
+        try {
+            const formData = new FormData();
+            console.log("fileList", fileList);
+            fileList.map((file, index) => {
+                if (file.id) {
+                    formData.append("old_images[]", file.id);
+
+                    formData.append(
+                        "old_title[]",
+                        values[`image_title_${file.id}`]
+                    );
+                    return;
+                }
+                if (file.uid) {
+                    formData.append("images[]", file.originFileObj);
+
+                    formData.append(
+                        "image_title[]",
+                        values[`image_title_${file.uid}`]
+                    );
+                }
+            });
+            formData.append("user_id", id);
+            await addDocument(formData);
+            message.success(`Document changed successfully`);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    if (isLoading) return <>Loading</>;
+
+    return (
+        <div className="content">
+            <Card
+                style={{ marginTop: "20px", height: "600px" }}
+                title="Upcoming Events"
+                extra={
+                    <Row
+                        justify="end"
+                        align=""
+                        style={{ marginBottom: "10px" }}
+                    >
+                        <Col>
+                            <Button
+                                type="primary"
+                                href="/events"
+                                icon={<FileAddOutlined />}
+                            >
+                                Create Event
+                            </Button>
+                        </Col>
+                    </Row>
+                }
+            >
+                <Calendar
+                    localizer={localizer}
+                    events={calendarData}
+                    startAccessor="start"
+                    endAccessor="end"
+                    views={["month", "agenda"]}
+                    defaultView="month" // Set the default view to "week"
+                    style={{ height: "400px" }}
+                    toolbar={true}
+                />
+            </Card>
+
+            {/* Staff List Table */}
+            <Card
+                style={{ marginTop: "20px" }}
+                title="My Staff List"
+                extra={
+                    <Button
+                        type="primary"
+                        onClick={() => navigate("/staff/create/" + user_id)}
+                    >
+                        Create
+                    </Button>
+                }
+            >
+                <Flex justify="flex-end" style={{ marginBottom: "10px" }}>
+                    <Col span={6}>
+                        <Input
+                            placeholder="Search..."
+                            addonBefore={<SearchOutlined />}
+                            value={str_search}
+                            onChange={(e) => OnSearchStr(e)}
+                        />
+                    </Col>
+                </Flex>
+                <Table
+                    columns={columns}
+                    dataSource={employee_list.map((employee) => ({
+                        key: employee.id,
+                        name: employee.name,
+                        email: employee.email,
+                        job_title: employee.employee_details?.title || "N/A",
+                        emp_picture: employee.employee_details?.emp_picture,
+                        id: employee.id,
+                    }))}
+                    pagination={false}
+                    rowKey="key"
+                />
+            </Card>
+
+            {/* HR Documents Table */}
+            <Card style={{ marginTop: "20px" }} title="General Documents">
+                <Flex justify="flex-end" style={{ marginBottom: "10px" }}>
+                    <Col span={6}>
+                        <Input
+                            placeholder="Search..."
+                            addonBefore={<SearchOutlined />}
+                            value={str_search}
+                            onChange={(e) => OnSearchStr(e)}
+                        />
+                    </Col>
+                </Flex>
+                <Form
+                    layout="vertical"
+                    form={form}
+                    initialValues={{}}
+                    onFinish={(values) => onFinish(values, user_id)}
+                >
+                    <Upload
+                        listType="picture"
+                        customRequest={handleDocumentRequest}
+                        multiple={true}
+                        fileList={fileList}
+                        showUploadList={false}
+                        onChange={handleDocumentChange}
+                    >
+                        <Button icon={<UploadOutlined />}>Upload</Button>
+                    </Upload>
+
+                    {fileList &&
+                        fileList.map((file) => (
+                            <Row
+                                gutter={[16, 16]}
+                                key={file.id}
+                                className="image_box_data"
+                                style={{ marginTop: "10px" }}
+                            >
+                                <Col span={3}>{ShowIcon(file)}</Col>
+                                <Col span={6}>
+                                    <Form.Item
+                                        style={{ margin: 0 }}
+                                        name={`image_title_${
+                                            file?.id || file.uid
+                                        }`}
+                                        initialValue={
+                                            file.id ? file.title : null
+                                        }
+                                    >
+                                        <Input
+                                            placeholder="Enter title"
+                                            required
+                                        />
+                                    </Form.Item>
+                                </Col>
+
+                                {file.id && (
+                                    <Col span={2}>
+                                        <Button
+                                            type="primary"
+                                            icon={<EyeOutlined />}
+                                        >
+                                            View
+                                        </Button>
+                                    </Col>
+                                )}
+                                {!file.id && <Col span={2}></Col>}
+                                <Col span={2}>
+                                    <Button
+                                        type="default"
+                                        icon={<DeleteOutlined />}
+                                        onClick={() =>
+                                            handleDeleteDocument(file)
+                                        }
+                                        color="danger"
+                                        variant="solid"
+                                        className="upload__img-close3"
+                                    >
+                                        Delete
+                                    </Button>
+                                </Col>
+                            </Row>
+                        ))}
+                    <Flex justify="flex-end">
+                        <Button
+                            type="default"
+                            style={{ marginRight: "10px" }}
+                            onClick={() => window.history.back()}
+                        >
+                            Discard
+                        </Button>
+                        <Button type="primary" htmlType="submit">
+                            Save
+                        </Button>
+                    </Flex>
+                </Form>
+            </Card>
+        </div>
+    );
+};
+
+export default Employee_listPage;
---
+++ resources/js/pages/staff/staff.detail.jsx
@@ -0,0 +1,307 @@
+import React, { useState } from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    useParams,
+} from "react-router-dom";
+
+import {
+    Form,
+    Input,
+    DatePicker,
+    Button,
+    Table,
+    Row,
+    Col,
+    Flex,
+    Card,
+    Upload,
+    Typography,
+    Spin,
+} from "antd";
+import { UploadOutlined, EyeOutlined, SearchOutlined } from "@ant-design/icons";
+import moment from "moment";
+
+const { Title } = Typography;
+
+import { useFetchEmployeeQuery } from "./staff.service";
+
+//this page is for client
+const MyStaffPage = ({}) => {
+    const { id } = useParams();
+    //id is employeeID
+    const { data, isLoading, isSuccess } = useFetchEmployeeQuery(
+        { employee_id: id },
+        {
+            refetchOnMountOrArgChange: true,
+        }
+    );
+    const [currentPage, setCurrentPage] = useState(1); // current page number
+    const [pageSize, setPageSize] = useState(5); // number of items per page
+
+    if (isLoading) return <Spin />;
+    const employee = data?.employee;
+    const employee_details = data?.employee.employee_details;
+    const employeeDocuments = data?.employee.employee_documents;
+    const columns = [
+        {
+            title: "#",
+            dataIndex: "index",
+            key: "index",
+            render: (text, record, index) => index + 1,
+        },
+        {
+            title: "Image",
+            dataIndex: "image",
+            key: "image",
+            render: (text, record) => {
+                const fileExtension = record.file
+                    ? record.file.split(".").pop()
+                    : "";
+                const image =
+                    fileExtension === "pdf"
+                        ? "/assets/images/pdf.png"
+                        : fileExtension === "doc" || fileExtension === "docx"
+                        ? "/assets/images/doc.jpg"
+                        : `/${record.file}`;
+                return (
+                    <img
+                        src={image}
+                        alt="document"
+                        className="rounded-pill"
+                        width="36"
+                        height="36"
+                    />
+                );
+            },
+        },
+        {
+            title: "Title",
+            dataIndex: "title",
+            key: "title",
+            render: (text) => (
+                <span>
+                    {text ? text.charAt(0).toUpperCase() + text.slice(1) : ""}
+                </span>
+            ),
+        },
+        {
+            title: "View",
+            key: "view",
+            render: (text, record) =>
+                record.file ? (
+                    <a
+                        href={`public/${record.file}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Button
+                            icon={<EyeOutlined />}
+                            size="large"
+                            type="primary"
+                        />
+                    </a>
+                ) : null,
+        },
+    ];
+    const totalCount = employeeDocuments?.length || 0;
+
+    const onPageChange = (page, pageSize) => {
+        setCurrentPage(page); // Update current page
+        setPageSize(pageSize); // Update page size if the user changes it
+    };
+    return (
+        <Card title="Staff Details">
+            <Form
+                layout="vertical"
+                initialValues={{
+                    ...employee,
+                    ...employee_details,
+                    date_of_birth: moment(employee_details.date_of_birth),
+                    date_of_joining: moment(employee_details.date_of_joining),
+                }}
+                onFinish={() => {}}
+            >
+                {/* Name */}
+                <Row gutter={[16, 16]}>
+                    <Col span={24}>
+                        <Form.Item label="Name" name="name">
+                            <Input disabled value={employee.name} />
+                        </Form.Item>
+                    </Col>
+
+                    {/* Email */}
+                    <Col span={24}>
+                        <Form.Item label="Email" name="email">
+                            <Input disabled value={employee.email} />
+                        </Form.Item>
+                    </Col>
+
+                    {/* Address */}
+                    <Col span={24}>
+                        <Form.Item label="Address" name="address">
+                            <Input disabled value={employee.address} />
+                        </Form.Item>
+                    </Col>
+
+                    {/* Phone Number */}
+                    <Col span={24}>
+                        <Form.Item label="Phone Number" name="phone_number">
+                            <Input disabled value={employee.phone_number} />
+                        </Form.Item>
+                    </Col>
+
+                    {/* Date of Birth */}
+                    <Col span={24}>
+                        <Form.Item label="Date of Birth" name="date_of_birth">
+                            <DatePicker
+                                disabled
+                                value={
+                                    employee.date_of_birth
+                                        ? moment(employee.date_of_birth)
+                                        : null
+                                }
+                            />
+                        </Form.Item>
+                    </Col>
+
+                    {/* Profile Picture */}
+                    <Col span={24}>
+                        <Form.Item label="Profile Picture">
+                            {employee.employee_details?.emp_picture ? (
+                                <img
+                                    src={`/${employee.employee_details.emp_picture}`}
+                                    alt="Profile"
+                                    width="100px"
+                                    height="100px"
+                                />
+                            ) : (
+                                <div>No Profile Picture</div>
+                            )}
+                        </Form.Item>
+                    </Col>
+
+                    {/* Other Details */}
+                    <Col span={24}>
+                        <Title level={5}>Other Details</Title>
+                    </Col>
+
+                    {/* Bank Name */}
+                    <Col span={24}>
+                        <Form.Item label="Bank Name" name="bank_name">
+                            <Input
+                                disabled
+                                value={employee.employee_details.bank_name}
+                            />
+                        </Form.Item>
+                    </Col>
+
+                    {/* Sort Code */}
+                    <Col span={24}>
+                        <Form.Item label="Sort Code" name="sort_code">
+                            <Input
+                                disabled
+                                value={employee.employee_details?.sort_code}
+                            />
+                        </Form.Item>
+                    </Col>
+
+                    {/* Account Number */}
+                    <Col span={24}>
+                        <Form.Item label="Account Number" name="account_number">
+                            <Input
+                                disabled
+                                value={
+                                    employee.employee_details?.account_number
+                                }
+                            />
+                        </Form.Item>
+                    </Col>
+
+                    {/* Title */}
+                    <Col span={24}>
+                        <Form.Item label="Title" name="title">
+                            <Input
+                                disabled
+                                value={employee.employee_details?.title}
+                            />
+                        </Form.Item>
+                    </Col>
+
+                    {/* Salary */}
+                    <Col span={24}>
+                        <Form.Item label="Salary" name="salary">
+                            <Input
+                                disabled
+                                value={employee.employee_details?.salary}
+                            />
+                        </Form.Item>
+                    </Col>
+
+                    {/* Direct Reports */}
+                    <Col span={24}>
+                        <Form.Item label="Direct Reports" name="direct_reports">
+                            <Input
+                                disabled
+                                value={
+                                    employee.employee_details?.direct_reports
+                                }
+                            />
+                        </Form.Item>
+                    </Col>
+
+                    {/* Date of Joining */}
+                    <Col span={24}>
+                        <Form.Item
+                            label="Date of Joining"
+                            name="date_of_joining"
+                        >
+                            <DatePicker
+                                disabled
+                                value={
+                                    employee.employee_details?.date_of_joining
+                                        ? moment(
+                                              employee.employee_details
+                                                  .date_of_joining
+                                          )
+                                        : null
+                                }
+                            />
+                        </Form.Item>
+                    </Col>
+
+                    {/* Documents */}
+                    <Col span={24}>
+                        <Title level={5}>Documents</Title>
+                        <Flex
+                            justify="flex-end"
+                            style={{ marginBottom: "10px" }}
+                        >
+                            <Col span={6}>
+                                <Input
+                                    placeholder="Search..."
+                                    addonBefore={<SearchOutlined />}
+                                />
+                            </Col>
+                        </Flex>
+                        <Table
+                            columns={columns}
+                            dataSource={employeeDocuments}
+                            pagination={{
+                                current: currentPage, // Current page number
+                                pageSize: pageSize, // Number of items per page
+                                total: totalCount, // Total number of items
+                                onChange: onPageChange,
+                                showSizeChanger: true, // Show size changer dropdown (optional)
+                                pageSizeOptions: ["5", "10", "20", "50"], // Page size options
+                            }}
+                            rowKey="id"
+                        />
+                    </Col>
+                </Row>
+            </Form>
+        </Card>
+    );
+};
+export default MyStaffPage;
